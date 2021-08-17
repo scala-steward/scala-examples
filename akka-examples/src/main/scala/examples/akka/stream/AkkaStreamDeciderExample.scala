049c4b663620e0fc82099898f6d96e2bb4386806
tatatakky <tt7t9kk9y1aaa@gmail.com>
2021-08-17 21:38:42 +0900

    fix: input data Seq[Divide] to Seq[Int]

--- akka-examples/src/main/scala/examples/akka/stream/AkkaStreamDeciderExample.scala
+++ akka-examples/src/main/scala/examples/akka/stream/AkkaStreamDeciderExample.scala
@@ -21,16 +21,20 @@ object AkkaStreamDeciderExample {
     case _                      => Supervision.Stop
   }
 
-  val source: Seq[Divide] => Source[Divide, NotUsed] = input => Source( input )
-  val flow: Flow[Divide, Int, NotUsed] = Flow[Divide].map {
-    case Divide(a, b) => a / b
+  val source: Seq[Int] => Source[Int, NotUsed] = input => Source( input )
+  val mkDivFlow: Flow[Int, Divide, NotUsed] = Flow[Int].map{ v =>
+      if(v % 2 == 0) Divide(v, 0)
+      else Divide(v, 1)
+  }
+  val divFlow: Flow[Divide, Int, NotUsed] = Flow[Divide].map {
+    case Divide(a, b) => println(a); a / b
   }.withAttributes(ActorAttributes.supervisionStrategy(decider))
   val sink: Sink[Int, Future[Int]] = Sink.fold(0)(_ + _)
 
-  def runnable(input: Seq[Divide]): RunnableGraph[Future[Int]] =
-    source(input).via(flow).toMat(sink)(Keep.right)
+  def runnable(input: Seq[Int]): RunnableGraph[Future[Int]] =
+    source(input).via(mkDivFlow).via(divFlow).toMat(sink)(Keep.right)
 
-  def execute(input: Seq[Divide]): Unit = {
+  def execute(input: Seq[Int]): Unit = {
     try {
       val sum: Future[Int] = runnable( input ).run()
       implicit val executionContext: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
@@ -43,5 +47,3 @@ object AkkaStreamDeciderExample {
     }
   }
 }
-
-// execute( Seq(Divide(2, 1), Divide(1, 0), Divide(6, 2), Divide(2, 0)) ) // 5
